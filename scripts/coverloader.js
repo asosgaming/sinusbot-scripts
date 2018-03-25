8cc52f6fd25d1d469d5dc75e1be08b4fbaf3c45e
Chad Reesey <reesey275@gmail.com>
2018-03-25 17:48:01 -0400

    updates

--- scripts/coverloader.js
+++ scripts/coverloader.js
@@ -1,46 +1,46 @@
+// @ts-check
 registerPlugin({
-    name: 'CoverLoader',
-    version: '1.0',
-    description: 'Update every cover art of playing tracks, even from radio stations. Powered by api.deezer.com',
-    author: 'Kevin Händel <kevinhaendel@gmail.com>',
+    name: 'Album Art',
+    version: '1.0.0',
+    backends: ['ts3'],
+    description: 'Changes Sinusbots Avatar to the Album cover for the currently playing track. This Plugin utilizes the public API from api.deezer.com',
+    author: 'Reesey275 <reesey275@asosgaming.com>',
     vars: [{
         name: 'setLocalTracksCoverArt',
         title: 'In addition to streams, do you want to set the cover art of local saved tracks?',
         type: 'select',
-        options: [
-            'No',
-            'Yes'
-        ]
+        options: ['No','Yes']
     },
     {
         name: 'updateLocalTracksCoverArt',
         title: 'If you enabled the option above, do you want to update the cover art if they changed?',
         type: 'select',
-        options: [
-            'No',
-            'Yes'
-        ]
+        options: ['No','Yes']
     },
     {
         name: 'setAsAvatar',
         title: 'Do you want to set the downloaded cover art as avatar? (If "No", Covatar won\'t set the updated cover art)',
         type: 'select',
-        options: [
-            'No',
-            'Yes'
-        ]
+        options: ['No','Yes']
     }]
 }, function(sinusbot, config) {
-    sinusbot.on('track', function(ev) {
+    // import modules
+    var engine = require("engine");
+    var event = require("event");
+    var ws = require('ws');
+    var backend = require('backend');
+    var api = "http://api.deezer.com/search?q=";
+
+    event.on('track', function(ev) {
         if (ev.type == '' || ev.type == 'track') {
             if (config.setLocalTracksCoverArt) {
                 if (!ev.thumbnail || config.updateLocalTracksCoverArt) {
                     if (ev.title != '' && ev.artist != '') {
                         var searchString = ev.artist.split(' feat.')[0] + ' - ' + ev.title.replace(new RegExp('`', 'g'), "'").replace(new RegExp('´', 'g'), "'");
-                        sinusbot.log('Searching cover art for ' + ev.artist + ' - ' + ev.title + ' ...');
+                        engine.log('Searching cover art for ' + ev.artist + ' - ' + ev.title + ' ...');
                         sinusbot.http({
                             method: 'GET',
-                            url: 'http://api.deezer.com/search?q=' + encodeURIComponent(searchString) + '&limit=1',
+                            url: api + encodeURIComponent(searchString) + '&limit=1',
                             timeout: 30000
                         }, function(error, response) {
                             if (typeof response != 'undefined' && response.statusCode == 200) {
@@ -54,9 +54,9 @@ registerPlugin({
                                                     sinusbot.setDefaultAvatar();
                                                 }
                                             }
-                                            sinusbot.log('Updated cover art!');
+                                            engine.log('Updated cover art!');
                                         } else {
-                                            sinusbot.log("Couldn't set cover art!");
+                                            engine.log("Couldn't set cover art!");
                                         }
                                     }
                                 }
@@ -68,14 +68,14 @@ registerPlugin({
         }
     });
     
-    sinusbot.on('trackInfo', function(ev) {
+    event.on('trackInfo', function(ev) {
         if (ev.type == 'url') {
             if (ev.tempTitle != '' && ev.tempArtist != '') {
                 var searchString = ev.tempArtist.replace(new RegExp('`', 'g'), "'").replace(new RegExp('´', 'g'), "'").replace(/ ?((\[|\().*feat\. ?.*(\]|\)))|( ?feat\..*)/g, '') + ' - ' + ev.tempTitle.replace(new RegExp('`', 'g'), "'").replace(new RegExp('´', 'g'), "'").replace(/ ?((\[|\().*feat\. ?.*(\]|\)))|( ?feat\..*)/g, '');
-                sinusbot.log('Searching cover art for ' + ev.tempArtist + ' - ' + ev.tempTitle + ' ...');
+                engine.log('Searching cover art for ' + ev.tempArtist + ' - ' + ev.tempTitle + ' ...');
                 sinusbot.http({
                     method: 'GET',
-                    url: 'http://api.deezer.com/search?q=' + encodeURIComponent(searchString) + '&limit=1',
+                    url: api + encodeURIComponent(searchString) + '&limit=1',
                     timeout: 30000
                 }, function(error, response) {
                     if (typeof response != 'undefined' && response.statusCode == 200) {
@@ -89,9 +89,9 @@ registerPlugin({
                                             sinusbot.setDefaultAvatar();
                                         }
                                     }
-                                    sinusbot.log('Updated cover art!');
+                                    engine.log('Updated cover art!');
                                 } else {
-                                    sinusbot.log("Couldn't set cover art!");
+                                    engine.log("Couldn't set cover art!");
                                 }
                             }
                         }
